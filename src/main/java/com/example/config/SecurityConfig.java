3cb871116a2fbe392c5d91ea5579cda155abe3fd
Nisanth2004 <nisanthjothi2004@gmail.com>
2024-11-12 07:09:59 +0530

    Role based Authentication

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -4,15 +4,21 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -25,5 +31,26 @@ public class SecurityConfig {
         http.sessionManagement(session->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+
+    // In memory Authentication
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+
+        UserDetails user1= User
+                .withUsername("user1")
+                .password("{noop}password1")
+                .roles("USER")
+                .build();
+
+        UserDetails admin= User.withUsername("admin").password("{noop}adminPass").roles("ADMIN").build();
+        /*
+           calling the constrcutor , you can view this in InMemoryUserDetailsManager
+           This contructor accepts the UserDetails Object so we can pass the inmemory user to this constrctor
+         */
+        return new InMemoryUserDetailsManager(user1,admin);
     }
 }
--- src/main/java/com/example/controller/GreetController.java
+++ src/main/java/com/example/controller/GreetController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
@@ -11,6 +12,22 @@ public class GreetController {
     public String hello()
     {
         return "hello";
+    }
+
+
+
+    @PreAuthorize("hasRole('USER')")// check authorizatiob of user
+    @GetMapping("/user")
+    public String user()
+    {
+        return "hello User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")// check authorizatiob of user
+    @GetMapping("/admin")
+    public String admin()
+    {
+        return "hello Admin";
     }
 
 }
