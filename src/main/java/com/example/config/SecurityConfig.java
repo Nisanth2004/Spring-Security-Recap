75b77862a1bb9f8a4c4c5b95173cfbca7461ebf1
Nisanth2004 <nisanthjothi2004@gmail.com>
2024-11-13 16:47:18 +0530

    JWT Implementation

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.config;
 
+import com.example.jwt.AuthEntryPointJwt;
+import com.example.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -27,15 +33,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter()
+    {
+        return new AuthTokenFilter();
+    }
 
     @Autowired
     DataSource dataSource;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll()
+        http.authorizeHttpRequests((requests) ->
+                requests
+                        .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/signin").permitAll()
                 .anyRequest().authenticated());
        http.formLogin(withDefaults());
-       http.httpBasic(withDefaults());
+
+       // custom exception handling mechanism
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+       //http.httpBasic(withDefaults());
        http.csrf(c->c.disable());
 
         // disable the session-id
@@ -43,45 +63,49 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.headers(header->header.frameOptions(frameOptionsConfig -> frameOptionsConfig.sameOrigin()));
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-
-    // In memory Authentication
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
     @Bean
-    public UserDetailsService userDetailsService()
-    {
+    public CommandLineRunner initData(UserDetailsService userDetailsService) {
 
-        UserDetails user1= User
-                .withUsername("user1")
-                .password(passwordEncoder().encode("password1"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin= User
-                .withUsername("admin")
-                .password(passwordEncoder().encode("adminPass"))
-                .roles("ADMIN").build();
-        /*
+        // In memory Authentication
+        return args -> {
+            JdbcUserDetailsManager manager = (JdbcUserDetailsManager) userDetailsService;
+            UserDetails user1 = User.withUsername("user1")
+                    .password(passwordEncoder().encode("password1"))
+                    .roles("USER")
+                    .build();
+            UserDetails admin = User.withUsername("admin")
+                    //.password(passwordEncoder().encode("adminPass"))
+                    .password(passwordEncoder().encode("adminPass"))
+                    .roles("ADMIN")
+                    .build();
+    /*
            calling the constrcutor , you can view this in InMemoryUserDetailsManager
            This contructor accepts the UserDetails Object so we can pass the inmemory user to this constrctor
          */
 
-
-        JdbcUserDetailsManager userDetailsManager=new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.createUser(user1);
-        userDetailsManager.createUser(admin);
-        return userDetailsManager;
-
-        //return new InMemoryUserDetailsManager(user1,admin);
+            JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+            userDetailsManager.createUser(user1);
+            userDetailsManager.createUser(admin);
+        };
     }
-
-
     // service Interface for Password encoder
 
     @Bean
     public PasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
-    }}
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
+    }
+}
--- src/main/java/com/example/controller/GreetController.java
+++ src/main/java/com/example/controller/GreetController.java
@@ -1,12 +1,37 @@
 package com.example.controller;
 
+import com.example.dto.LoginRequest;
+import com.example.dto.LoginResponse;
+import com.example.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
 public class GreetController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @GetMapping("/hello")
     public String hello()
@@ -15,19 +40,46 @@ public class GreetController {
     }
 
 
-
-    @PreAuthorize("hasRole('USER')")// check authorizatiob of user
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
-    public String user()
-    {
-        return "hello User";
+    public String userEndpoint(){
+        return "Hello, User!";
     }
 
-    @PreAuthorize("hasRole('ADMIN')")// check authorizatiob of user
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
-    public String admin()
-    {
-        return "hello Admin";
+    public String adminEndpoint(){
+        return "Hello, Admin!";
+    }
+
+
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        } catch (AuthenticationException exception) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        LoginResponse response = new LoginResponse(userDetails.getUsername(), roles, jwtToken);
+
+        return ResponseEntity.ok(response);
     }
 
 }
